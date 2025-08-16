f8bc55cded35c056397e85f930c93b21e9d1abd2
austin-eaquinto <157089105+austin-eaquinto@users.noreply.github.com>
2025-08-16 12:31:45 -0600

    commented out sections of code to check for run status of the program

--- core/src/main/java/io/github/roger18gm/java2dgame/movement/ChaseMovement.java
+++ core/src/main/java/io/github/roger18gm/java2dgame/movement/ChaseMovement.java
@@ -1,19 +1,19 @@
-package io.github.roger18gm.java2dgame.movement;
-
-import io.github.roger18gm.java2dgame.entities.Entity;
-import io.github.roger18gm.java2dgame.entities.Player;
-
-public class ChaseMovement implements Movement {
-    private Player player;
-
-    public ChaseMovement(Player player) {
-        this.player = player;
-    }
-
-    public void move(Entity entity) {
-        int dx = Integer.compare(player.getX(), entity.getX());
-        int dy = Integer.compare(player.getY(), entity.getY());
-        entity.setX(entity.getX() + dx * entity.getSpeed());
-        entity.setY(entity.getY() + dy * entity.getSpeed());
-    }
-}
+//package io.github.roger18gm.java2dgame.movement;
+//
+//import io.github.roger18gm.java2dgame.entities.Entity;
+//import io.github.roger18gm.java2dgame.entities.Player;
+//
+//public class ChaseMovement implements Movement {
+//    private Player player;
+//
+//    public ChaseMovement(Player player) {
+//        this.player = player;
+//    }
+//
+//    public void move(Entity entity) {
+//        int dx = Integer.compare(player.getX(), entity.getX());
+//        int dy = Integer.compare(player.getY(), entity.getY());
+//        entity.setX(entity.getX() + dx * entity.getSpeed());
+//        entity.setY(entity.getY() + dy * entity.getSpeed());
+//    }
+//}
--- core/src/main/java/io/github/roger18gm/java2dgame/movement/PlayerMovement.java
+++ core/src/main/java/io/github/roger18gm/java2dgame/movement/PlayerMovement.java
@@ -1,27 +1,27 @@
-package io.github.roger18gm.java2dgame.movement;
-
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import io.github.roger18gm.java2dgame.entities.Entity;
-
-public class PlayerMovement implements Movement {
-    public void move(Entity entity) {
-        int dx = getInputX(); // Assume a method that reads input
-        int dy = getInputY();
-        entity.setX(entity.getX() + dx * entity.getSpeed());
-        entity.setY(entity.getY() + dy * entity.getSpeed());
-    }
-
-    private int getInputX() {
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) return -1;
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) return 1;
-        return 0;
-    }
-
-    private int getInputY() {
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) return -1;
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) return 1;
-        return 0;
-    }
-
-}
+//package io.github.roger18gm.java2dgame.movement;
+//
+//import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.Input;
+//import io.github.roger18gm.java2dgame.entities.Entity;
+//
+//public class PlayerMovement implements Movement {
+//    public void move(Entity entity) {
+//        int dx = getInputX(); // Assume a method that reads input
+//        int dy = getInputY();
+//        entity.setX(entity.getX() + dx * entity.getSpeed());
+//        entity.setY(entity.getY() + dy * entity.getSpeed());
+//    }
+//
+//    private int getInputX() {
+//        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) return -1;
+//        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) return 1;
+//        return 0;
+//    }
+//
+//    private int getInputY() {
+//        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) return -1;
+//        if (Gdx.input.isKeyPressed(Input.Keys.UP)) return 1;
+//        return 0;
+//    }
+//
+//}
